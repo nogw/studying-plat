0593665434bfc3e487cc0ce37e277c83ae22d885
nogw <gabrielnogueiraoliveira@gmail.com>
2021-07-25 22:01:03 -0300

    (fix) challenge route

--- backend/src/controllers/challenge.user.controller.ts
+++ backend/src/controllers/challenge.user.controller.ts
@@ -68,11 +68,30 @@ const sendChallengeSolve = async (req: Request, res: Response) => {
     return res.status(404).json({
       error: "Challenge not found."
     })
-  } else {
-    const objCompletedChallenges = { idChallenge: req.body.challengeId, completedAt: req.body.time }
+  } 
+  
+  try {
+    const challenge = new challengeUserModel({
+      challengeId: req.body.challengeId,
+      userId: req.body.userId,
+      solution: req.body.solution,
+      time: req.body.time
+    })
+    
+    challenge.save()
+    .catch((error: string | object) => {
+      return res.status(400).json({
+        error: error
+      })
+    })
+
     User.findOneAndUpdate(
       { _id: req.body.userId },
-      { $push: { completedChallenges: objCompletedChallenges, approved: "await" } },
+      { $push: { 
+          completedChallenges: { idChallenge: req.body.challengeId, completedAt: req.body.time }, 
+          approved: "await" 
+        }
+      },
       { upsert: true },
       (error: any, doc: any) => {
         if (error) {
@@ -84,26 +103,6 @@ const sendChallengeSolve = async (req: Request, res: Response) => {
         }
       }
     )
-  }
-
-  try {
-    const challenge = new challengeUserModel({
-      challengeId: req.body.challengeId,
-      userId: req.body.userId,
-      solution: req.body.solution,
-      time: req.body.time
-    })
-    challenge.save()
-    .then(() => {
-      return res.status(200).json({
-        message: "Solution sent"
-      })
-    })
-    .catch((error: string | object) => {
-      return res.status(400).json({
-        error: error
-      })
-    })
   } catch (error) {
     return res.status(400).json({
       error: error
