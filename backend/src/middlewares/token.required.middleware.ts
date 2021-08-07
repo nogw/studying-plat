f98cf21949527031b4d5999d207b97f21ff9daee
nogw <gabrielnogueiraoliveira@gmail.com>
2021-08-06 22:02:37 -0300

    (fix) token middleware

--- backend/src/middlewares/token.required.middleware.ts
+++ backend/src/middlewares/token.required.middleware.ts
@@ -1,7 +1,7 @@
-import { NextFunction, Request } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import Jwt from '../lib/jwt.lib'
 
-const token_validator = async (req: Request, next: NextFunction) => {
+const token_validator = async (req: Request, _: Response, next: NextFunction) => {
   const payload = await Jwt.decode(req.headers.authorization!);
   req.body.token_payload = payload || { _id: null };
   return next();
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,12 +3,14 @@ import express from 'express'
 import cors from 'cors'
 import router from './router'
 import db from './db'
+import token_validator from './middlewares/token.required.middleware'
 
 export const app = express()
 const port = process.env.PORT || 8000
 
 app.use(express.json())
 app.use(cors())
+app.use(token_validator)
 app.use("/", router)
 
 db.connect()
