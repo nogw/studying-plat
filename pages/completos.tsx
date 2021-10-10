2d123c1a04e163ee6e1612bef945232d600cbc11
nogw <gabrielnogueiraoliveira@gmail.com>
2021-10-09 22:18:23 -0300

    list complete challenges

--- pages/completos.tsx
+++ pages/completos.tsx
@@ -13,7 +13,7 @@ function Completos({ challenges }) {
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const response: any = await api.get(`/challenge/user/challenges/6160e47eef372319884f034c`)
-  const challenges = response.data.message[0]
+  const challenges = response.data.message[0].completedChallenges
   console.log(challenges)
 
   return {
