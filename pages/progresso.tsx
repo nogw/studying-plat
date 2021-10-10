e38f92adef91fc4cc3622a9ea9036d1989666359
nogw <gabrielnogueiraoliveira@gmail.com>
2021-10-09 22:19:12 -0300

    list in progress challenges

--- pages/progresso.tsx
+++ pages/progresso.tsx
@@ -13,7 +13,7 @@ function Progresso({ challenges }) {
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const response: any = await api.get(`/challenge/user/challenges/6160e47eef372319884f034c`)
-  const challenges = response.data.message[0]
+  const challenges = response.data.message[0].inProgressChallenges
   console.log(challenges)
 
   return {
