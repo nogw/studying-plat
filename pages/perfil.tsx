cf9c02bbc27ae19ed326f14a9fb822c2034f12a5
nogw <gabrielnogueiraoliveira@gmail.com>
2021-10-10 22:27:44 -0300

    profile page

---
+++ pages/perfil.tsx
@@ -0,0 +1,12 @@
+import Layout from "../src/components/Layout";
+import ProfileComponent from "../src/components/ProfileComponent";
+
+function Perfil() {
+  return (
+    <Layout>
+      <ProfileComponent/>
+    </Layout>
+  );
+}
+
+export default Perfil;
