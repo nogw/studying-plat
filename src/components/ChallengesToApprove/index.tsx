c7fbca15b1ce5eb2e488154a0fc068cdaa876ed4
nogw <gabrielnogueiraoliveira@gmail.com>
2021-10-11 13:16:30 -0300

    component to list challenges

---
+++ src/components/ChallengesToApprove/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from './styles';
+
+function ChallengesToApprove() {
+  return (
+    <Container>
+      <h1>ChallengesToApprove</h1>
+    </Container>
+  );
+};
+
+export default ChallengesToApprove;
---
+++ src/components/ChallengesToApprove/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #202225;
+  padding: 12px;
+  border-radius: 8px;
+  height: 100%;
+`;
