b1c1d01f26581bc064343bbc92ee3bb4cee0c95f
nogw <gabrielnogueiraoliveira@gmail.com>
2021-10-12 22:34:23 -0300

    create a popup to view challenges and approve or reject

--- src/components/AdminListChallenges/styles.ts
+++ src/components/AdminListChallenges/styles.ts
@@ -4,10 +4,15 @@ export const Container = styled.div`
   background-color: #202225;
   padding: 12px;
   border-radius: 8px;
-  height: 100%;
+  min-width: 300px;
   overflow: auto;
-
+  
   h1 {
     color: red;
+  }
+  
+  @media screen and (max-width: 750px) {
+    min-height: 500px;
+    width: 100%;
   }
 `;
