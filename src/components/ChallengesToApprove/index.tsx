436fff0e8b2838d04fff37aa584faa3512be9156
nogw <gabrielnogueiraoliveira@gmail.com>
2021-10-12 22:34:04 -0300

    create a popup to view challenges and approve or reject

--- src/components/ChallengesToApprove/index.tsx
+++ src/components/ChallengesToApprove/index.tsx
@@ -1,34 +1,86 @@
-import { Container, ItemListS } from './styles';
+import { AnimatePresence } from 'framer-motion';
+import { useState } from 'react';
+import { api } from '../../utils/api';
+import { Bgc } from '../Navbar/styles';
+import { Container, ItemListS, PopUpChallenge } from './styles';
+import materialOceanic from '../Challenge/theme'
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 
-function ItemList({ id, challengeId, time }) {
+function PopUpChallengeToApprove({ challenge }) {
+  const setChallenge = async (set) => {
+    try {
+      const response = await api.post("/challenge/admin/set", {
+        set: set,
+        challengeId: challenge.challengeId,
+        userId: challenge.userId
+      })
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <ItemListS>
-      <div className="square"/>
-      <div className="texts">
-        <h2>{challengeId}</h2>
-        <h5>{time}</h5>
+    <PopUpChallenge>
+      <h1>user id: {challenge?.userId}</h1>
+      <h2>sent on: {challenge?.time}</h2>
+      <SyntaxHighlighter  
+        style={materialOceanic} 
+        language="python" 
+        PreTag="div"  
+        children={challenge?.solution}
+      />
+      <div className="bts">
+        <button onClick={() => setChallenge(true)} className="button approve">APPROVE</button>
+        <button onClick={() => setChallenge(false)} className="button reject">REJECT</button>
       </div>
-    </ItemListS>
+    </PopUpChallenge>
   )
 }
 
-function ChallengesToApprove({challengesToApprove}) {
+function ChallengesToApprove({ challengesToApprove }) {
+  const [challenge, setChallenge] = useState([])
+  const [show, setShow] = useState(false)
+
+  const getChallenge = async (userId, id) => {
+    const response: any = await api.get(`/challenge/user/challenge/${userId}/${id}`)
+    setChallenge(response.data.message)
+    setShow(true)
+  }
+
   return (
-    <Container>
-      <div className="challenges">
-        {
-          challengesToApprove?.map(v => {
-            return (
-              <ItemList 
-                id={v._id} 
-                challengeId={v.challengeId} 
-                time={v.time}
-              />
-            )
-          })
-        }
-      </div>
-    </Container>
+    <>
+      {
+        show && (
+          <AnimatePresence>
+            <PopUpChallengeToApprove challenge={challenge}/>
+            <Bgc
+              onClick={() => setShow(false)}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+            />
+          </AnimatePresence>
+        )
+      }
+      <Container>
+        <div className="challenges">
+          {
+            challengesToApprove?.map(v => {
+              return (
+                <ItemListS key={v._id} onClick={() => getChallenge(v.userId, v.challengeId)}>
+                  <div className="square"/>
+                  <div className="texts">
+                    <h2>{v.challengeId}</h2>
+                    <h5>{v.time}</h5>
+                  </div>
+                </ItemListS>
+              )
+            })
+          }
+        </div>
+      </Container>
+    </>
   );
 };
 
--- src/components/ChallengesToApprove/styles.ts
+++ src/components/ChallengesToApprove/styles.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
   background-color: #202225;
   padding: 12px;
   border-radius: 8px;
-  height: 100%;
+  overflow: hidden;
 
   .challenges {
     height: 100%;
@@ -12,6 +12,65 @@ export const Container = styled.div`
     overflow: auto;
     gap: 12px;
     flex-direction: column;
+  }
+
+  @media screen and (max-width: 750px) {
+    min-height: 500px;
+  }
+`;
+
+export const PopUpChallenge = styled.div`
+  z-index: 999;
+  position: fixed;
+  width: 90%;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #202225;
+  padding: 20px;
+  border-radius: 6px;
+
+  h1, h2 {
+    background-color: #25282b;
+    padding: 12px;
+    word-wrap: break-word;
+    font-family: "Roboto Mono";
+    font-size: 16px;
+    color: #a7a7a9;
+  }
+
+  h2 {
+    margin-top: 12px;
+    margin-bottom: 32px;
+  }
+
+  .bts {
+    display: flex;
+    margin-top: 12px;
+    gap: 12px;
+
+    .button {
+      padding: 8px 8px 9px 8px;
+      background-color: transparent;
+      cursor: pointer;
+      border-radius: 4px;
+      outline: none;
+      width: 100%;
+      
+      font-family: Roboto;
+      border: 2px solid #a7a7a9;
+      color: #a7a7a9;
+      font-weight: 900;
+      transition: 200ms ease all;
+    }
+    .approve:hover {
+      color: #6cba6b;
+      border : 2px solid #6cba6b;
+    }
+    .reject:hover {
+      color: #e63946;
+      border : 2px solid #e63946;
+    }
   }
 `;
 
