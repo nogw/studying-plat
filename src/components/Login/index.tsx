7527268800374a745a0ed6aeda5c6ffe44a7647d
nogw <gabrielnogueiraoliveira@gmail.com>
2021-10-14 22:27:26 -0300

    integrate login and save token in cookies

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,26 +1,56 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 import NProgress from 'nprogress'; 
+import { setCookie } from 'nookies'
 
 import { 
   Container,
   Input 
 } from './styles';
+import { api } from '../../utils/api';
+import Router from 'next/router'
 
 interface IErrors {
   emailError?: string,
   passwordError?: string,
 }
 
-const handleLogin = () => {
-  NProgress.start()
-  NProgress.done()
-}
-
 function Login({ setComponentNow }) {
   const [errors, setErrors] = useState<IErrors>({
     passwordError: "required",
   })
+
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  })
+
+  const handleLogin = async () => {
+    NProgress.start()
+
+    try {
+      const response: any = await api.post("/user/login", {
+        email: form.email,
+        password: form.password
+      })
+
+      setCookie(null, 'py.plat.user.id', response.data.message, {
+        maxAge: 30 * 24 * 60 * 60,
+        path: "/"
+      })
+
+      Router.push("/")
+    } catch (error) {
+      console.log(error)      
+    }
+
+    NProgress.done()
+  }
+
+  const updateForm = e => {
+    const { name, value } = e.target
+    setForm(prev => ({...prev, [name]: value }))
+  }
 
   useEffect(() => {
     setErrors({})
@@ -29,7 +59,7 @@ function Login({ setComponentNow }) {
   return (
     <Container>
       <Input error={errors.emailError}>
-        <input type="text" name="email"/>
+        <input onChange={(e) => updateForm(e)} type="text" name="email"/>
         <label htmlFor="email">
           Email 
           {
@@ -38,7 +68,7 @@ function Login({ setComponentNow }) {
         </label>
       </Input>
       <Input error={errors.passwordError}>
-        <input type="text" name="password"/>
+        <input onChange={(e) => updateForm(e)} type="password" name="password"/>
         <label htmlFor="password">
           Password
           {
