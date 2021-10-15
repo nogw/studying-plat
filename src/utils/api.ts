5ca29a42d3f2be1e7af0069b733ad6427093c5a5
nogw <gabrielnogueiraoliveira@gmail.com>
2021-10-14 22:25:03 -0300

    get authorization token from cookies

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
+import { parseCookies } from "nookies"
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL, 
 })
 
-api.defaults.headers.common["authorization"] = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNWY4OTQ4YjRhNjMxMWI1NDIwODVjMyIsIm5hbWUiOiJub2d3IiwicGVybWlzc2lvbiI6ImFkbWluIiwiaWF0IjoxNjMzNjUxMzY4LCJleHAiOjE2MzM3Mzc3Njh9.72lbTXUmvV0OuccsfwEpgEF1UiX30SQxzfy1R39He3o"
+const cookies = parseCookies()
+api.defaults.headers.common["authorization"] = cookies
